57af6d06ee0d8553304f6f1b1c1abad822b2f300
RandyChen <jushanc2@illinois.edum>
2023-11-11 22:03:33 -0600

    admm mpc is not working yet

--- main.py
+++ main.py
@@ -15,19 +15,21 @@ from time import strftime
 from dynamics import *
 import util
 from admm_mpc import *
-
+from mpc import *
 
 if __name__ == "__main__":
-    convex_problem = True
+    convex = True
+    
+    x0,xr = util.paper_setup_3_quads()
     # x0,xr = util.setup_3_quads()
-    x0, xr = util.setup_5_quads()
+    # x0, xr = util.setup_5_quads()
     # x0, xr = util.four_quad_exchange()
-    T = 15
-    radius = 0.4
+    
+    T = 10 # MPC horizon length
+    radius = 0.3
     n_states = 6
     n_inputs = 3
-    # n_agents = 3
-    n_agents = 5
+    n_agents = 3
     ids = [100+n for n in range(n_agents)]
     Q = np.eye(n_states*n_agents)*1
     for i in range(n_agents):
@@ -35,20 +37,38 @@ if __name__ == "__main__":
 
     R = np.eye(n_inputs*n_agents)*0.1
     Qf = Q*100
-    # ADMM_ITER = 10
-    ADMM_ITER = 5
+    ADMM_ITER = 10
+    # ADMM_ITER = 5
     
+    # X_full, U_full, obj_trj, mean_time, obj_history = solve_distributed_rhc(ids, n_states, \
+    #                                                                         n_inputs, n_agents, \
+    #                                                                         x0, xr, T, radius, \
+    #                                                                         Q, R, Qf, ADMM_ITER, \
+    #                                                                         convex_problem=convex,
+    #                                                                         n_trial=None)
     
-    """Run convex version first:"""
-    X_full, U_full, obj_trj, mean_time, obj_history = solve_distributed_rhc(ids, n_states, \
-                                                                            n_inputs, n_agents, \
-                                                                            x0, xr, T, radius, \
-                                                                            Q, R, Qf, ADMM_ITER, \
-                                                                            convex_problem=True,
-                                                                            n_trial=None)
+    # np.savez("distributed_BVC_convex_{}.npz".format(n_agents), X_full=X_full, obj_trj=obj_trj, obj_hist = obj_history, xr=xr)
     
-    np.savez("ADMM_BVC_convex_{}.npz".format(n_agents), X_full=X_full, obj_trj=obj_trj, obj_hist = obj_history, xr=xr)
+    X_full, U_full, obj_trj, mean_time, obj_history = solve_admm_mpc(n_states, 
+                                                                     n_inputs, 
+                                                                     n_agents, 
+                                                                     x0, 
+                                                                     xr, 
+                                                                     T, 
+                                                                     radius, 
+                                                                     Q, 
+                                                                     R, 
+                                                                     Qf, 
+                                                                     ADMM_ITER, n_trial=None)
+    np.savez("admm_consensus_BVC_convex_{}.npz".format(n_agents), X_full=X_full, obj_trj=obj_trj, obj_hist = obj_history, xr=xr)
     
+    # X_trj, U_trj, obj_trj, mean_times, obj_hist = solve_mpc_centralized(n_agents,
+    #                                                                     x0, xr, 
+    #                                                                     T, radius, 
+    #                                                                     Q, R, Qf, 
+    #                                                                     30, 
+    #                                                                     )
+    # np.savez("centralized_BVC_convex_{}.npz".format(n_agents), X_full=X_trj, obj_trj=obj_trj, obj_hist = obj_hist, xr=xr)
     
 #     """Run non-convex version"""
 #     X_full_nonConvex, U_full_nonConvex, \
